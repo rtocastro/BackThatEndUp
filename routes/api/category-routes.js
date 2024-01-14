e1e7aa57d50d56a83a4a530833b2c9e7737c610d
rtocastro <ricktorres@live.com>
2024-01-13 21:10:06 -0800

    trying to make post route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -12,25 +12,40 @@ router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
   try {
-    Category.findOne({
-      where: {
-        id: req.params.id,
-      },
-      include: [Product],
-    })
+    const categoryData = Category.findByOne(req.params.id, {
+      include: [{ model: Product }],
+    });
 
-  }catch (err) {
-    console.log(err);
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category with that ' });
+      return;
+    }
+
+    res.status(200).json(idData);
+  } catch (err) {
     res.status(500).json(err);
   }
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    id: req.body.id,
+    category_name: req.body.category_name
+  })
+    .then((newCategory) => {
+      // Send the newly created row as a JSON object
+      res.json(newCategory);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
+
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  
 });
 
 router.delete('/:id', (req, res) => {
